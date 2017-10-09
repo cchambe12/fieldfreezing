bbbb4280dc797d5b38661cffc1a958349a68c07b
cchambe12 <cchamberlain@g.harvard.edu>
2017-10-09 13:14:27 -0400

    updated list of individuals tagged

---
+++ Tagged_HF.R
@@ -0,0 +1,24 @@
+## 09 October 2017 - Cat
+# Duration of Vegetative Risk: field data
+# Aim: To track which species to include in experiment
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Set Working directory
+setwd("~/Documents/git/fieldfreezing")
+d<-read.csv("Exp_taggedHF.csv", header=TRUE)
+
+d$species<-substr(d$ind, 3,8)
+d$number<-substr(d$ind, 12, 13)
+d$stage<-substr(d$ind, 0,1)
+d$sp_stage<-substr(d$ind, 0,8)
+
+count<-as.data.frame(table(d$sp_stage))
